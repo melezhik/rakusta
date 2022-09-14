808d299222a8231130880dc7a79695181db9d2dc
Alexey Melezhik <melezhik@gmail.com>
2022-09-13 20:58:25 -0500

    add debug

--- .tom/tasks/run-docker/task.bash
+++ .tom/tasks/run-docker/task.bash
@@ -3,6 +3,7 @@ name=$(config name)
 status=$(docker inspect --format '{{json .State.Running}}' $name)
 
 if [[ -n $string ]] || [[ $status != "true" ]] ; then
+  set -x
   docker run  --rm -td \
   --add-host=host.docker.internal:host-gateway \
   --name $name $name
